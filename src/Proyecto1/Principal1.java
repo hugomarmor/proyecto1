f8b90def8b54673268812540d92aa9f4b29c055b
Hugo <hugo.marmor@educa.jcyl.es>
2023-04-10 19:14:52 +0200

    commit clase principal1

--- src/Proyecto1/Principal1.java
+++ src/Proyecto1/Principal1.java
@@ -5,4 +5,5 @@ public class Principal1 {
 	System.out.println("Hola Git");
 	System.out.println("Hola Git 2");
 	System.out.println("Hola Git 3");
+	System.out.println("Pruebas Git. Se añade cambio1");
 }
